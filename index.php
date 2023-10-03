9b7af6ea4965c88b516343f174af8dcf9c4cc6f7
Danila Nazarov <danila.nazarov.2004@mail.ru>
2023-10-03 20:17:03 +0400

    mark

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juisables</title>
+    <link rel="stylesheet" href="/style/main.css">
+    <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Merriweather:300,400,400i,700&amp;subset=cyrillic-ext" rel="stylesheet">
+</head>
+<body>
+    
+<nav class="navi">
+
+    <div>
+        <a href="" class="logo_text" link="#000000" vlink="000000">
+            Juisables
+        </a>
+    </div>
+
+    <div>
+        <a href="" class="links" link="#000000" vlink="000000">
+            Условия Доставки
+        </a>
+    </div>
+
+  <div>
+        <a href="" class="links" link="#000000" vlink="000000">
+            Контакты
+        </a>
+    </div>
+
+    <div>
+        <a href="auth.php" class="links" link="#000000" vlink="000000">
+            Регистрация
+        </a>
+    </div>
+
+    <div>
+        <a href="auth.php" class="links" link="#000000" vlink="000000">
+            Вход
+        </a>
+    </div>
+
+    <button class="nav__toggle-menu">
+        <span></span>
+    </button>
+
+    </nav>
+
+    <header class="header">
+		
+            <p>
+                Получай Свежие <br>
+                Juisables<br>
+                Каждый День
+            </p>
+			
+		<div class="header_subtitle">
+            <p>
+               Всего 360р/мес.<br>
+               И вы останетесь довольны
+            </p>
+            </div>
+
+
+	</header>
+
+</body>
+</html>
