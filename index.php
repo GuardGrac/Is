2e6256775d1c9bb9fd154002b4a2a8f69b602c26
Danila Nazarov <danila.nazarov.2004@mail.ru>
2024-01-11 20:57:02 +0400

    last

--- index.php
+++ index.php
@@ -62,14 +62,18 @@
             </p>
 			
 		<div class="header_subtitle">
+
             <p>
                Всего 360р/мес.<br>
                И вы останетесь довольны
             </p>
-            </div>
+
+        </div>
 
             <button class="order_button">
+
                 Заказать
+
             </button>
     
 	</header>
@@ -87,8 +91,10 @@
                     <big><big><big>10000+</big></big></big> <br>
                         
                         <small>
+
                             Experience design is the design of anything,
                             independent of medium.
+
                         </small>
                     </p>
                     
@@ -105,8 +111,10 @@
                     <big><big><big>10000+</big></big></big> <br>
                         
                         <small>
+
                             Experience design is the design of anything,
                             independent of medium.
+
                         </small>
                     </p>
                     
@@ -123,8 +131,10 @@
                     <big><big>10000+</big></big> <br>
                         
                         <small>
+
                             Experience design is the design of anything,
                             independent of medium.
+
                         </small>
                     </p>
                     
@@ -137,23 +147,31 @@
          <div class="text_about">
 
             <H1> 
+
                  Наша работа
+
             </H1>
 
             <p>
+
             Experience design is the design of anything,
                     independent of medium, or across media,
                     and human engagement as an explicit goal.
+
             </p>
 
             <p>
+
             Experience design is the design of anything,
                     independent of medium, or across media,
                     and human engagement as an explicit goal.
+
             </p>
 
             <button class="order_button">
+
                 Заказать
+
             </button>
 
          </div>
@@ -164,8 +182,250 @@
     <section class="about-us">
 
         <div class="block-about">
+
+            <div class="frst-row">
+
+                <img src="/icons/assortment.webp" alt="" class="icons-about-us">
+
+                <p class="text-title-about">
+                    
+                    Широкий ассортимент
+
+                </p>
+
+            </div>
+
+             <p class="description-about-us">
+
+                Сайт Juisables предоставляет большой выбор различных видов соков, что позволяет покупателям выбирать из множества вариантов в соответствии с их предпочтениями и потребностями
+                
+            </p>
+
+            <button class="about-us-button">
+
+                Это про меня!
+
+                <svg class="arrow" version="1.1" id="Layer_1" xmlns:x="&ns_extend;" xmlns:i="&ns_ai;" xmlns:graph="&ns_graphs;"
+                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="96px" height="96px"
+                    viewBox="0 0 96 96" enable-background="new 0 0 96 96" xml:space="preserve">
+                    <switch>
+                        <g i:extraneous="self">
+                            <path d="M44,12v62.344L22.828,53.172c-1.562-1.562-4.094-1.562-5.656,0c-1.562,1.562-1.562,4.095,0,5.657l28,28
+                                c1.562,1.562,4.095,1.562,5.656,0l28-28C79.609,58.048,80,57.024,80,56c0-1.023-0.391-2.047-1.172-2.828
+                                c-1.562-1.562-4.095-1.562-5.656,0L52,74.344V12c0-2.208-1.791-4-4-4S44,9.791,44,12z"/>
+                        </g>
+                    </switch>
+                </svg>
+                
+            </button>
             
         </div>
+        
+        <div class="block-about">
+            
+            <div class="frst-row">
+
+                <img src="/icons/like.webp" alt="" class="icons-about-us">
+
+                <p class="text-title-about">
+
+                    Качество продукции
+
+                </p>
+
+            </div>
+
+            <p class="description-about-us">
+
+                Juisables может гарантировать высокое качество своих соков, что может быть подтверждено отзывами клиентов и сертификатами качества
+                
+            </p>
+
+            <button class="about-us-button">
+
+                Это про меня!
+
+                <svg class="arrow" version="1.1" id="Layer_1" xmlns:x="&ns_extend;" xmlns:i="&ns_ai;" xmlns:graph="&ns_graphs;"
+                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="96px" height="96px"
+                    viewBox="0 0 96 96" enable-background="new 0 0 96 96" xml:space="preserve">
+                    <switch>
+                        <g i:extraneous="self">
+                            <path d="M44,12v62.344L22.828,53.172c-1.562-1.562-4.094-1.562-5.656,0c-1.562,1.562-1.562,4.095,0,5.657l28,28
+                                c1.562,1.562,4.095,1.562,5.656,0l28-28C79.609,58.048,80,57.024,80,56c0-1.023-0.391-2.047-1.172-2.828
+                                c-1.562-1.562-4.095-1.562-5.656,0L52,74.344V12c0-2.208-1.791-4-4-4S44,9.791,44,12z"/>
+                        </g>
+                    </switch>
+                </svg>
+                
+            </button>
+            
+        </div>
+
+        <div class="block-about">
+            
+            <div class="frst-row">
+
+                <img src="/icons/monitor.webp" alt="" class="icons-about-us">
+                    
+                <p class="text-title-about">
+
+                    Удобство покупки
+
+                </p>
+
+            </div>
+
+            <p class="description-about-us">
+
+                Покупка соков на сайте Juisables может быть выполнена в несколько кликов, что экономит время и усилия покупателей
+
+            </p>
+
+            <button class="about-us-button">
+
+                Это про меня!
+
+                <svg class="arrow" version="1.1" id="Layer_1" xmlns:x="&ns_extend;" xmlns:i="&ns_ai;" xmlns:graph="&ns_graphs;"
+                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="96px" height="96px"
+                    viewBox="0 0 96 96" enable-background="new 0 0 96 96" xml:space="preserve">
+                    <switch>
+                        <g i:extraneous="self">
+                            <path d="M44,12v62.344L22.828,53.172c-1.562-1.562-4.094-1.562-5.656,0c-1.562,1.562-1.562,4.095,0,5.657l28,28
+                                c1.562,1.562,4.095,1.562,5.656,0l28-28C79.609,58.048,80,57.024,80,56c0-1.023-0.391-2.047-1.172-2.828
+                                c-1.562-1.562-4.095-1.562-5.656,0L52,74.344V12c0-2.208-1.791-4-4-4S44,9.791,44,12z"/>
+                        </g>
+                    </switch>
+                </svg>
+                
+            </button>
+            
+        </div>
+
+    </section>
+
+    <section class="about-us">
+
+        <div class="block-about">
+            
+            <div class="frst-row">
+
+                <img src="/icons/support.webp" alt="" class="icons-about-us">
+
+                <p class="text-title-about">
+
+                    Поддержка 24/7
+
+                </p>
+
+            </div>
+
+             <p class="description-about-us">
+
+                    Интересующий тебя вопрос ты, сможешь задать в любое для тебя время
+
+            </p>
+
+            <button class="about-us-button">
+
+                Это про меня!
+
+                <svg class="arrow" version="1.1" id="Layer_1" xmlns:x="&ns_extend;" xmlns:i="&ns_ai;" xmlns:graph="&ns_graphs;"
+                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="96px" height="96px"
+                    viewBox="0 0 96 96" enable-background="new 0 0 96 96" xml:space="preserve">
+                    <switch>
+                        <g i:extraneous="self">
+                            <path d="M44,12v62.344L22.828,53.172c-1.562-1.562-4.094-1.562-5.656,0c-1.562,1.562-1.562,4.095,0,5.657l28,28
+                                c1.562,1.562,4.095,1.562,5.656,0l28-28C79.609,58.048,80,57.024,80,56c0-1.023-0.391-2.047-1.172-2.828
+                                c-1.562-1.562-4.095-1.562-5.656,0L52,74.344V12c0-2.208-1.791-4-4-4S44,9.791,44,12z"/>
+                        </g>
+                    </switch>
+                </svg>
+                
+            </button>
+            
+        </div>
+
+        <div class="block-about">
+            
+            <div class="frst-row">
+
+                <img src="/icons/top.webp" alt="" class="icons-about-us">
+
+                <p class="text-title-about">
+
+                    Один из лучших
+
+                </p>
+
+            </div>
+
+            <p class="description-about-us">
+
+                Сайт Juisables является одним из самых лучших сайтов по продаже соков по меркам нашей аналитики
+                
+            </p>
+
+            <button class="about-us-button">
+
+                Это про меня!
+
+                <svg class="arrow" version="1.1" id="Layer_1" xmlns:x="&ns_extend;" xmlns:i="&ns_ai;" xmlns:graph="&ns_graphs;"
+                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="96px" height="96px"
+                    viewBox="0 0 96 96" enable-background="new 0 0 96 96" xml:space="preserve">
+                    <switch>
+                        <g i:extraneous="self">
+                            <path d="M44,12v62.344L22.828,53.172c-1.562-1.562-4.094-1.562-5.656,0c-1.562,1.562-1.562,4.095,0,5.657l28,28
+                                c1.562,1.562,4.095,1.562,5.656,0l28-28C79.609,58.048,80,57.024,80,56c0-1.023-0.391-2.047-1.172-2.828
+                                c-1.562-1.562-4.095-1.562-5.656,0L52,74.344V12c0-2.208-1.791-4-4-4S44,9.791,44,12z"/>
+                        </g>
+                    </switch>
+                </svg>
+                
+            </button>
+            
+        </div>
+
+        <div class="block-about">
+            
+            <div class="frst-row">
+
+                <img src="/icons/verified.webp" alt="" class="icons-about-us">
+
+                <p class="text-title-about">
+
+                    Все данные защищенны
+
+                </p>
+
+            </div>
+
+            <p class="description-about-us">
+
+                Все ваши данные предоставленные нашему сайту полностью защищенны
+
+            </p>
+
+            <button class="about-us-button">
+
+                Это про меня!
+
+                <svg class="arrow" version="1.1" id="Layer_1" xmlns:x="&ns_extend;" xmlns:i="&ns_ai;" xmlns:graph="&ns_graphs;"
+                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="96px" height="96px"
+                    viewBox="0 0 96 96" enable-background="new 0 0 96 96" xml:space="preserve">
+                    <switch>
+                        <g i:extraneous="self">
+                            <path d="M44,12v62.344L22.828,53.172c-1.562-1.562-4.094-1.562-5.656,0c-1.562,1.562-1.562,4.095,0,5.657l28,28
+                                c1.562,1.562,4.095,1.562,5.656,0l28-28C79.609,58.048,80,57.024,80,56c0-1.023-0.391-2.047-1.172-2.828
+                                c-1.562-1.562-4.095-1.562-5.656,0L52,74.344V12c0-2.208-1.791-4-4-4S44,9.791,44,12z"/>
+                        </g>
+                    </switch>
+                </svg>
+                
+            </button>
+            
+        </div>
+                    
+    </div>
 
     </section>
 
